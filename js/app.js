ad8fcc432cf9198ea2c832ab1d38c2bd06c2f3e5
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-01 23:17:06 +0100

    EDIT client info on ticket

--- js/app.js
+++ js/app.js
@@ -6,42 +6,85 @@ const inputKmElement = document.getElementById('km-section')
 
 const inputAgeElement = document.getElementById('age-section')
 
-// console.log (inputNameElement, kmTravel)
-
 const btnSendElement = document.querySelector('.btn-send')
 
 const btnSendCancel = document.querySelector('.btn-cancel')
 
-const discount65 = parseInt(inputKmElement.value) * 0.4
-
-const discountUnderage = parseInt(inputKmElement.value) * 0.2
-
-// console.log (btnSendElement, btnSendCancel)
-
-// 1. selezionare nome utente
-// 2.selexionare chilometri corsa
 // 3.selezionare fascia eta
 
 btnSendElement.addEventListener('click', function() {
 
+    // 1. selezionare nome utente
+
     let namePassenger = inputNameElement.value
+
+    // 2.selexionare chilometri corsa
 
     let kilometerTravel = parseInt(inputKmElement.value)
 
+    // console.log(kilometerTravel)    
+
+    const pricePerKm = 0.21
+
+    // console.log(pricePerKm)
+
+    let kmPrice = kilometerTravel * pricePerKm
+
+    // 3.selezionare fascia eta
+
+    let kmPriceDiscounted  = document.getElementById('ticket-discount')
+
     let agePassenger = inputAgeElement.value
 
-    switch (agePassenger) {
-        case 'nessuna' : 'inputKmElement.value'
-        break
+    if (agePassenger === 'minorenne') {
+        kmPriceDiscounted = kmPrice * 0.8;
 
-        case 'minorenne' : 'inputKmElement.value - discountUnderage'
-        break
+    }else if(agePassenger === 'adulto') {
+        kmPriceDiscounted = kmPrice;
 
-        case 'over65' : 'inputKmElement.value - discount65'
+    }else if(agePassenger === 'over-65') {
+        kmPriceDiscounted = kmPrice * 0.6;
     }
+    
+    console.log *kmPriceDiscounted
+    // .5 spamapre su biglietto i dati
+
+    let ticketName = namePassenger
+
+    ticketName = document.getElementById('ticket-name')
+
+    ticketName.innerHTML =  namePassenger
+
+    let ticketKm = kilometerTravel
+
+    ticketKm = document.getElementById('ticket-km')
+
+    ticketKm.innerHTML = kilometerTravel
+
+    let ticketAge = agePassenger
+
+    ticketAge = document.getElementById('ticket-age')
+
+    ticketAge.innerHTML = agePassenger
+
+    let ticketPrice  
+
+    ticketPrice = document.getElementById('ticket-price')
+
+    ticketPrice.innerHTML = (kmPrice).toFixed(2)
+
+    kmPriceDiscounted.innerHTML = (kmPriceDiscounted).toFixed(2)
+
+    console.log(kmPriceDiscounted)
+
+    
+
+
+
+
+    
 })
 
-console.log(btnSendElement)
 
 
 
