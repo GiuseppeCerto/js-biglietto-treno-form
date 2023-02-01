5e6c319dbed505fa02a396a0037103b4f7b6a24c
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-01 15:31:42 +0100

    EDIT simple layout

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// 1. selezionare nome utente
+
+const inputNameElement = document.querySelector(#name-section input[name="credentials"])
+
+const kmTravel =  document.querySelectro(input [name="kilometers"])
+
+console.log (inputNameElement, kmTravel)
+
+// 2.selexionare chilometri corsa
+// 3.selezionare fascia eta
+// 4.moltiplicare i chilometri per il prezzo al km
+// 4.aggiungere sconto se min. o over65
