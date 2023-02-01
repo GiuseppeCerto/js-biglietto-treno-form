c624fe667b29ee73ec23ea0ce251d3b419d647c4
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-01 16:35:51 +0100

    EDIT event click

--- js/app.js
+++ js/app.js
@@ -1,12 +1,50 @@
+// 0. dichiarare costanti
+
+const inputNameElement = document.getElementById('name-section')
+
+const inputKmElement = document.getElementById('km-section')
+
+const inputAgeElement = document.getElementById('age-section')
+
+// console.log (inputNameElement, kmTravel)
+
+const btnSendElement = document.querySelector('.btn-send')
+
+const btnSendCancel = document.querySelector('.btn-cancel')
+
+const discount65 = parseInt(inputKmElement.value) * 0.4
+
+const discountUnderage = parseInt(inputKmElement.value) * 0.2
+
+// console.log (btnSendElement, btnSendCancel)
+
 // 1. selezionare nome utente
-
-const inputNameElement = document.querySelector(#name-section input[name="credentials"])
-
-const kmTravel =  document.querySelectro(input [name="kilometers"])
-
-console.log (inputNameElement, kmTravel)
-
 // 2.selexionare chilometri corsa
 // 3.selezionare fascia eta
+
+btnSendElement.addEventListener('click', function() {
+
+    let namePassenger = inputNameElement.value
+
+    let kilometerTravel = parseInt(inputKmElement.value)
+
+    let agePassenger = inputAgeElement.value
+
+    switch (agePassenger) {
+        case nessuna : 'inputKmElement.value'
+        break
+
+        case minorenne : 'inputKmElement.value - discountUnderage'
+        break
+
+        case over65 : 'inputKmElement.value - discount65'
+    }
+})
+
+console.log(namePassenger, kilometerTravel, agePassenger, btnSendElement)
+
+
+
+
 // 4.moltiplicare i chilometri per il prezzo al km
 // 4.aggiungere sconto se min. o over65
